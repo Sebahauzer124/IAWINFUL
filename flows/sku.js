536217d41108ecf69f4248d9181446cc36751345
Sebastian Hauzer <sebahauzer124@gmail.com>
2025-07-10 12:09:23 -0300

    pum

--- flows/sku.js
+++ flows/sku.js
@@ -49,7 +49,8 @@ module.exports = async function flujoSku(incomingMsg, from, estadoConversacion) 
       'SUPER PREMIUM',
       'STELLA',
       'ANDES',
-      'UNG'
+      'UNG',
+      'MARKETPLACE',
     ]);
 
     const mejorSkuPorSegmento = {};
